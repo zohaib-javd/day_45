7650fd3d1d248faf4717de5e36f7f2439102c5be
Zohaib Javed <zohaibjaved@gmail.com>
2024-04-15 08:24:00 +0500

    Questions 133, 134 and 135

---
+++ README.md
@@ -0,0 +1,3 @@
+"# day_45" 
+
+#100DaysOfCodingChallenge
---
+++ main.ts
@@ -0,0 +1,29 @@
+// Question 133: Write a JavaScript object and convert it into a JSON string.
+
+const person = {
+    name: "Zohaib",
+    age: 39,
+    city: "Karachi",
+};
+
+const jsonstring = JSON.stringify(person);
+
+console.log(jsonstring);
+
+console.log("\n");
+
+// Question 134: Take a JSON string and parse it into a JavaScript object.
+
+const jsonstring1 = '{"name":"Zohaib","age":39,"city":"Karachi"}';
+
+const person1 = JSON.parse(jsonstring1);
+
+console.log(person1);
+
+console.log("\n");
+
+// Question 135: Explain how you can format a JSON string with proper indentation for readability.
+
+const jsonstring2 = JSON.stringify(person, null, 2);
+
+console.log(jsonstring2);
